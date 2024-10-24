97b2e8f63e6b9d452ec3ec14121f1713ca914947
251558 Kosowski Mateusz <251558@edu.p.lodz.pl>
2024-10-24 12:49:36 +0200

    Mod gitignore, del idea, add GameOfLifeCell

---
+++ src/main/java/org/team1/GameOfLifeCell.java
@@ -0,0 +1,38 @@
+package org.team1;
+
+public class GameOfLifeCell {
+    private boolean value;
+    private GameOfLifeCell[] neighbors;
+
+    GameOfLifeCell(boolean value) {
+        this.value = value;
+    }
+
+    void setNeighbors(GameOfLifeCell[] newNeighbors) {
+        this.neighbors = newNeighbors;
+    }
+
+    // Jak nic nie ma to dostęp jest domyślny, czyli tylko w pakiecie
+    boolean getValue() {
+        return value;
+    }
+
+    boolean nextState() {
+        int aliveNeighbors = 0;
+        for (GameOfLifeCell neighbor : neighbors) {
+            if (neighbor.getValue()) {
+                aliveNeighbors++;
+            }
+        }
+        if (value) {
+            return aliveNeighbors == 2 || aliveNeighbors == 3;
+        } else {
+            return aliveNeighbors == 3;
+        }
+    }
+
+    void updateState(boolean newState) {
+        value = newState;
+    }
+
+}
