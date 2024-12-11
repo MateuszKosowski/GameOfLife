af5f97a9b3c172e3e9efae6ab4e91ae293f9ac29
251558 Kosowski Mateusz <251558@edu.p.lodz.pl>
2024-12-11 17:28:06 +0100

    Add input settings fields

--- View/src/main/java/org/view/GOLController.java
+++ View/src/main/java/org/view/GOLController.java
@@ -1,8 +1,8 @@
 package org.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.util.converter.IntegerStringConverter;
 
 
 // Ta klasa odpowiada za:
@@ -11,12 +11,48 @@ import javafx.scene.control.TextField;
 // Implementację logiki aplikacji.
 public class GOLController {
 
-    // Adnotacja @FXML oznacza, że pole będzie wstrzykiwane przez JavaFX.
     @FXML
-    // TextField to pole tekstowe, w którym użytkownik może wprowadzać tekst.
     private TextField widthField;
-    
+
     @FXML
     private TextField heightField;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private RadioButton option1;
+
+    @FXML
+    private RadioButton option2;
+
+    @FXML
+    private RadioButton option3;
+
+    @FXML
+    private ToggleGroup groupOfLife;
+
+    @FXML
+    public void initialize() {
+        confirmButton.setOnAction(e -> {
+            minMaxSize(widthField);
+            minMaxSize(heightField);
+            System.out.println("Width: " + widthField.getText() + " Height: " + heightField.getText());
+        });
+    }
+
+    private void minMaxSize(TextField field) {
+        String input = field.getText();
+        try {
+            int size = Integer.parseInt(input);
+            if (size >= 4 && size <= 20) {
+                field.setText(String.valueOf(size));
+            } else {
+                field.setText(String.valueOf(4));
+            }
+        } catch (NumberFormatException e) {
+            field.setText(String.valueOf(4));
+        }
+    }
 
 }
